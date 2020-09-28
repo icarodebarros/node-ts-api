9309964851e0e780cb4f439517d8eaae46cb7b68
Icaro de Barros <icarodebarros@gmail.com>
2020-09-28 12:18:13 -0300

    Instalação e uso do OvernightJS e ajustes de teste

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { Server } from '@overnightjs/core';
+import './util/module-alias';
+import * as express from 'express';
+import { ForecastController } from './controllers/forecast';
+
+export class SetupServer extends Server {
+    
+    constructor(private port = 3000) {
+        super();
+    }
+
+    public init(): void {
+        this.setupExpress();
+        this.setupControllers();
+    }
+
+    public getApp(): express.Application {
+        return this.app;
+    }
+
+    private setupExpress(): void {
+        this.app.use(express.json());
+    }
+
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+        this.addControllers([forecastController]);
+    }
+}
