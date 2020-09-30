eea339675f9afe46548a8899dbe2bb3f73b6e5cb
Icaro de Barros <icarodebarros@gmail.com>
2020-09-30 09:14:48 -0300

    Instalação do Config para configurações dinâmicas

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from '@src/util/errors/internal-error';
 import { AxiosStatic } from 'axios';
+import config, { IConfig } from 'config';
 
 export interface StormGlassPointSource {
     [key: string]: number;
@@ -49,6 +50,8 @@ export class StormGlassResponseError extends InternalError {
     }
 }
 
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass');
+
 export class StormGlass {
     readonly stormGlassAPIParams =
         'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -58,11 +61,12 @@ export class StormGlass {
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
         try {
+            const apiUrl = stormGlassResourceConfig.get('apiUrl');
             const response = await this.request.get<StormGlassForecastResponse>(
-                `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+                `${apiUrl}/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
                 {
                     headers: {
-                        Authorization: 'fake-token',
+                        Authorization: stormGlassResourceConfig.get('apiToken'),
                     },
                 }
             );
