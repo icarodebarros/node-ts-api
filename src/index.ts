1bdfcb009795225cce92a54c533b5f9389fc7c62
Icaro de Barros <icarodebarros@gmail.com>
2020-10-01 11:46:30 -0300

    Instalando e configurando MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+import { SetupServer } from './server';
 console.log('test');
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,25 @@ import { Server } from '@overnightjs/core';
 import './util/module-alias';
 import * as express from 'express';
 import { ForecastController } from './controllers/forecast';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
+    /*
+    * same as this.port = port, declaring as private here will
+    * add the port variable to the SetupServer instance
+    */
     constructor(private port = 3000) {
         super();
     }
 
-    public init(): void {
+    /*
+    * We use a different method to init instead of using the constructor
+    * this way we allow the server to be used in tests and normal initialization
+    */
+    public async init(): Promise<void> {
         this.setupExpress();
         this.setupControllers();
+        await this.databaseSetup();
     }
 
     public getApp(): express.Application {
@@ -24,5 +34,13 @@ export class SetupServer extends Server {
     private setupControllers(): void {
         const forecastController = new ForecastController();
         this.addControllers([forecastController]);
+    }
+
+    private async databaseSetup(): Promise<void> {
+        await database.connect();
+    }
+    
+    public async close(): Promise<void> {
+        await database.close();
     }
 }
